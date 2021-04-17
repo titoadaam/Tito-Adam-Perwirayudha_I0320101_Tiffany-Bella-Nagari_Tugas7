8fe30fcc0b7ecd90795c22e25b5ed243068b9bf1
titoadaam <perwirayudhaa@gmail.com>
2021-04-17 08:10:54 +0700

    push

--- I0320101_Soal2_Tugas7.py
+++ I0320101_Soal2_Tugas7.py
@@ -1,26 +1,13 @@
-nilai_siswa = input("Masukkan nilai siswa (dipisahkan dengan spasi) : ").split()
+import math
 
-#Konversi menjadi tipe int
+nilai_siswa = input("Input nilai siswa (dipisahkan dengan spasi) : ").split()
 for i in range(len(nilai_siswa)):
     nilai_siswa[i] = int(nilai_siswa[i])
-
-#Mencari rata-rata nilai
 rerata = sum(nilai_siswa)/len(nilai_siswa)
-
 print("Nilai yang diinput : ", nilai_siswa)
 print('='*30)
-
-import math
-#Fungsi Numerik 1 = mencari nilai tertinggi
-print("Nilai yang paling tinggi adalah ", max(nilai_siswa))
-
-#Fungsi Numerik 2 = mencari nilai terendah
-print("Nilai yang paling rendah adalah ", min(nilai_siswa))
-
-print("Rata-rata nilai siswa adalah ", rerata)
-
-#Fungsi Numerik 3 = pembulatan ke atas
-print("Rata-rata nilai dengan pembulatan ke atas adalah ", math.ceil(rerata))
-
-#Fungsi Numerik 4 = pembulatan ke bawah
-print("Rata-rata nilai dengan pembulatan ke bawah adalah ", math.floor(rerata))
+print("Nilai yang paling tinggi ", max(nilai_siswa))
+print("Nilai yang paling rendah ", min(nilai_siswa))
+print("Rata-rata nilai siswa ", rerata)
+print("Rata-rata nilai dengan pembulatan ke atas ", math.ceil(rerata))
+print("Rata-rata nilai dengan pembulatan ke bawah  ", math.floor(rerata))
