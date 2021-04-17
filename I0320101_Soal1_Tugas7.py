512c4f7e82b88328e1ab3a732f03bc53c00e96cf
titoadaam <perwirayudhaa@gmail.com>
2021-04-17 08:08:06 +0700

    push

---
+++ I0320101_Soal1_Tugas7.py
@@ -0,0 +1,29 @@
+print("""
+
+Program Penggunaan String
+
+""")
+b = "Output program"
+print("Program upper dan lower, Berfungsi mengubah semua member jadi kapital")
+b2 = b.upper()
+print(b2)
+print()
+print("Mengubah member")
+b = "Mari kita coba"
+print(b)
+print("Misal mau mengubah ")
+print("Kita Ubah Mari = ", b.replace("Mari", "Ayo"))
+print()
+print("Program lower berfungsi mengubah semua huruf menjadi huruf kecil")
+b = "OUTPUT DARI PROGRAM"
+b3 = b.lower()
+print("Sebelum")
+print(b)
+print("Sesudah:")
+print(b3)
+print()
+str = "output dari program"
+a = str.capitalize()
+print("Program capitalize")
+print(a)
+
